aeee146f0cd55d0fa2ca59a23a34e2779a77d1ce
DerekRivers <dyk3rz@googlemail.com>
2017-10-26 10:06:37 +0100

    added css transistions

---
+++ source/App.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+import AnimatedShoppingList from './AnimatedShoppingList';
+
+render(<AnimatedShoppingList/>, document.getElementById('root'));
